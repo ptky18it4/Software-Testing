3350cd7b04755e89fe5a6e04bbe913615fbdc392
TungThanhHoang <61379007+TungThanhHoang@users.noreply.github.com>
2021-06-03 14:34:39 +0700

    Fund Transfer

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+    private By btnFundTransfer = By.linkText("Fund Transfer");
+    private By btnCustomStatement = By.linkText("Customised Statement");
+    public HomePage(WebDriver driver){
+        this.driver= driver;
+    }
+    public FundTransfer clickForm(){
+         driver.findElement(btnFundTransfer).click();
+         return new FundTransfer(driver);
+    }
+    public CustomStatement clickFormCustom(){
+        driver.findElement(btnCustomStatement).click();
+        return new CustomStatement(driver);
+    }
+
+}
---
+++ src/test/java/customStatement/testCustomStatement.java
@@ -0,0 +1,2 @@
+package customStatement;public class testCustomStatement {
+}
