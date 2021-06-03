429626b3109b747d2c5a9136e6b7af4922b9435e
TungThanhHoang <61379007+TungThanhHoang@users.noreply.github.com>
2021-06-03 14:38:07 +0700

    Customised Statement

--- src/main/java/pages/CustomStatement.java
+++ src/main/java/pages/CustomStatement.java
@@ -1,2 +1,63 @@
-package pages;public class CustomStatement {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CustomStatement {
+    private WebDriver driver;
+    private By accountNo = By.name("accountno");
+    private By formDate = By.name("fdate");
+    private By toDate = By.name("tdate");
+    private By miniumTransaction = By.name("amountlowerlimit");
+    private By numberTranscation = By.name("numtransaction");
+    private By btnSubmit = By.name("AccSubmit");
+    private By btnReset = By.name("res");
+    private By btnHome = By.linkText("Home");
+    public CustomStatement(WebDriver driver){
+        this.driver = driver;
+    }
+    public void setAccountNo(String a){
+        driver.findElement(accountNo).sendKeys(a);
+    }
+    public void setFormDate(String b){
+        driver.findElement(formDate).sendKeys(b);
+    }
+    public void setToDate(String c){
+        driver.findElement(toDate).sendKeys(c);
+    }
+    public void setMiniumTransaction(String d){
+        driver.findElement(miniumTransaction).sendKeys(d);
+    }
+    public void setNumberTranscation(String e){
+        driver.findElement(numberTranscation).sendKeys(e);
+    }
+    public String getMessageAccountNo(){
+        return driver.findElement(accountNo).getText();
+    }
+    public String getMessageFormDate(){
+         return driver.findElement(formDate).getText();
+    }
+    public String getMessageToDate(){
+        return driver.findElement(toDate).getText();
+    }
+    public String getMessageMiniumTransaction(){
+        return driver.findElement(miniumTransaction).getText();
+    }
+    public String getMessageNumberTransaction(){
+        return driver.findElement(numberTranscation).getText();
+    }
+    public void clickReset(){
+        driver.findElement(btnReset).click();
+    }
+    public void clickBtnHome(){
+        driver.findElement(btnHome).click();
+
+    }
+    public CustomStatementDetails clickSubmit (){
+        driver.findElement(btnSubmit).click();
+        return new CustomStatementDetails(driver);
+    }
+
+
+
 }
--- src/main/java/pages/CustomStatementDetails.java
+++ src/main/java/pages/CustomStatementDetails.java
@@ -1,2 +1,24 @@
-package pages;public class CustomStatementDetails {
+package pages;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CustomStatementDetails {
+    private WebDriver driver;
+
+    private By btnContinue = By.linkText("Continue");
+    public CustomStatementDetails(WebDriver driver){
+        this.driver = driver;
+    }
+    public HomePage clickBtnContinue(){
+        driver.findElement(btnContinue).click();
+        return new HomePage(driver);
+    }
+    public String getAlertText(){
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        alert.accept();
+        return alertText;
+    }
 }
