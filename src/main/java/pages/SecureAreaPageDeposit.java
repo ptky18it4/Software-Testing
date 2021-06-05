bab70353eedb0cc814260162a8c531ab92fe4823
Trung Ky <phamtrungky14022000@gmail.com>
2021-06-05 15:04:59 +0700

    modified code'

--- src/main/java/pages/SecureAreaPageDeposit.java
+++ src/main/java/pages/SecureAreaPageDeposit.java
@@ -1,16 +1,13 @@
 package pages;
 
 import org.openqa.selenium.Alert;
-<<<<<<< HEAD
 import org.openqa.selenium.By;
-=======
->>>>>>> main
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 
 public class SecureAreaPageDeposit {
     WebDriver driver;
-<<<<<<< HEAD
+
     /***   Successe  */
     private By detailDeposit = By.cssSelector("table.layout:nth-child(9) table:nth-child(1) tbody:nth-child(1) tr:nth-child(1) td:nth-child(1) > p.heading3");
 
@@ -19,17 +16,14 @@ public class SecureAreaPageDeposit {
     public SecureAreaPageDeposit(WebDriver driver) {
         this.driver = driver;
     }
+
+
     /***   Successe  */
-    public String getTextDetail(){
+    public String getTextDetail() {
         String text = driver.findElement(detailDeposit).getText();
         return text;
     }
-=======
 
-    public SecureAreaPageDeposit(WebDriver driver) {
-        this.driver = driver;
-    }
->>>>>>> main
 
     public String getAlertText() {
         try {
